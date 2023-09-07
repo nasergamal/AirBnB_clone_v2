5c8dcb71a69040165815bd19c90b4b0f2311f920
nasergamal <naser.gamal98@gmail.com>
2023-09-07 23:34:10 +0300

    puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,11 +1,11 @@
-# prepare webstatic
+# config
 include stdlib
 
-exec { 'one nginx right away':
-  command  => 'sudo apt-get update; sudo apt-get -y install nginx',
-  provider => shell,
-  before   => Exec['first_dir']
 
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt',
+  before   => Exec['first_dir']
 }
 
 exec {'first_dir':
@@ -16,13 +16,15 @@ exec {'first_dir':
 exec {'sec_dir':
   command  => 'sudo mkdir -p /data/web_static/shared/',
   provider => shell,
-  before   => Exec['html']
+  before   => File['html']
 }
-exec {'html':
-  command  => "echo 'new config' | sudo tee /data/web_static/releases/test/index.html &> /dev/null",
-  provider => shell,
-  before   => Exec['sym']
+file { 'html':
+  ensure  => 'present',
+  path    => '/data/web_static/releases/test/index.html',
+  content => 'new config',
+  before  => Exec['sym']
 }
+
 exec {'sym':
   command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
   provider => shell,
@@ -37,7 +39,7 @@ exec {'owner':
 
 file_line { 'www':
   path   => '/etc/nginx/sites-enabled/default',
-  line   => "listen 80 default_server\n\tlocation /hbnb_static { alias /data/web_static/current/;",
+  line   => "listen 80 default_server\n\tlocation /hbnb_static { alias /data/web_static/current/; }",
   match  => 'listen 80 default_server',
   before => Exec['restart']
 }
