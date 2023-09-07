1573c1f5b2ae0aa6e96d2d0278d17b697cde10c7
nasergamal <naser.gamal98@gmail.com>
2023-09-08 01:56:14 +0300

    puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,50 +1,70 @@
 # config
-include stdlib
 
+$conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 https://www.nassera.tech;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
 
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt',
   before   => Exec['first_dir']
 }
+-> file { '/data':
+  ensure  => 'directory'
+}
 
-exec {'first_dir':
-  command  => 'sudo mkdir -p /data/web_static/releases/test/',
-  provider => shell,
-  before   => Exec['sec_dir']
+-> file { '/data/web_static':
+  ensure => 'directory'
 }
-exec {'sec_dir':
-  command  => 'sudo mkdir -p /data/web_static/shared/',
-  provider => shell,
-  before   => File['html']
+
+-> file { '/data/web_static/releases':
+  ensure => 'directory'
 }
-file { 'html':
+
+-> file { '/data/web_static/releases/test':
+  ensure => 'directory'
+}
+
+-> file { '/data/web_static/shared':
+  ensure => 'directory'
+}
+
+-> file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  path    => '/data/web_static/releases/test/index.html',
-  content => 'new config',
-  before  => Exec['sym']
+  content => 'new config'
+}
+-> file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
 }
 
-exec {'sym':
-  command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
-  provider => shell,
-  before   => Exec['owner']
+-> exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-exec {'owner':
-  command  => 'sudo chown -R ubuntu:ubuntu /data/',
-  provider => shell,
-  before   => File_line['www']
+-> file {'www':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  content => $conf,
 }
 
-file_line { 'www':
-  path   => '/etc/nginx/sites-enabled/default',
-  line   => "listen 80 default_server\n\tlocation /hbnb_static { alias /data/web_static/current/; }",
-  match  => 'listen 80 default_server',
-  before => Exec['restart']
-}
-
-exec {'restart':
+-> exec {'restart':
   command  => 'sudo service nginx restart',
   provider => shell
 }
